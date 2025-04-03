8120d8bb89f2643b37db047a2be170c288981645
Agrendalath <piotr@surowiec.it>
2025-04-03 20:36:57 +0200

    refactor: use `getConfig` instead of directly checking `process.env`

--- src/learningpath/data/api.js
+++ src/learningpath/data/api.js
@@ -1,20 +1,21 @@
 import { getAuthenticatedHttpClient, getAuthenticatedUser } from '@edx/frontend-platform/auth';
+import { getConfig } from '@edx/frontend-platform';
 
 export async function fetchLearningPaths() {
   const client = getAuthenticatedHttpClient();
-  const response = await client.get(`${process.env.LMS_BASE_URL}/api/learning_paths/v1/learning-paths/`);
+  const response = await client.get(`${getConfig().LMS_BASE_URL}/api/learning_paths/v1/learning-paths/`);
   return response.data.results || response.data;
 }
 
 export async function fetchLearningPathDetail(key) {
   const client = getAuthenticatedHttpClient();
-  const response = await client.get(`${process.env.LMS_BASE_URL}/api/learning_paths/v1/learning-paths/${key}/`);
+  const response = await client.get(`${getConfig().LMS_BASE_URL}/api/learning_paths/v1/learning-paths/${key}/`);
   return response.data;
 }
 
 export async function fetchLearningPathProgress(key) {
   const client = getAuthenticatedHttpClient();
-  const response = await client.get(`${process.env.LMS_BASE_URL}/api/learning_paths/v1/${key}/progress/`);
+  const response = await client.get(`${getConfig().LMS_BASE_URL}/api/learning_paths/v1/${key}/progress/`);
   return response.data;
 }
 
@@ -22,9 +23,9 @@ export async function fetchCourses(courseId) {
   const client = getAuthenticatedHttpClient();
   let url;
   if (courseId) {
-    url = `${process.env.LMS_BASE_URL}/api/courses/v1/courses/${encodeURIComponent(courseId)}/`;
+    url = `${getConfig().LMS_BASE_URL}/api/courses/v1/courses/${encodeURIComponent(courseId)}/`;
   } else {
-    url = `${process.env.LMS_BASE_URL}/api/courses/v1/courses/`;
+    url = `${getConfig().LMS_BASE_URL}/api/courses/v1/courses/`;
   }
   const response = await client.get(url);
   if (courseId) {
@@ -43,7 +44,7 @@ export async function fetchCourses(courseId) {
 export async function fetchCourseDetails(courseId) {
   const client = getAuthenticatedHttpClient();
   const response = await client.get(
-    `${process.env.STUDIO_BASE_URL}/api/contentstore/v1/course_details/${encodeURIComponent(courseId)}`,
+    `${getConfig().STUDIO_BASE_URL}/api/contentstore/v1/course_details/${encodeURIComponent(courseId)}`,
   );
   return response.data;
 }
@@ -63,7 +64,7 @@ export async function fetchCourseCompletion(courseId) {
   const { username } = getAuthenticatedUser();
   const client = getAuthenticatedHttpClient();
   const response = await client.get(
-    `${process.env.LMS_BASE_URL}/completion-aggregator/v1/course/${encodeURIComponent(courseId)}/?username=${username}`,
+    `${getConfig().LMS_BASE_URL}/completion-aggregator/v1/course/${encodeURIComponent(courseId)}/?username=${username}`,
   );
   if (response.data.results && response.data.results.length > 0) {
     return response.data.results[0].completion.percent;
